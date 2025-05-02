0a76c6391c421c51eff610421177b069703fc74f
prettyboy <prettyboymiron@yandex.ru>
2025-05-02 18:25:32 +0300

    yoy

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,10 +2,11 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = 'True'
 
